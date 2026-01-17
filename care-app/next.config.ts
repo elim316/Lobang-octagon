314344f2b5007cb51b80a1d4b9c477c968f43f9f
elim316 <2200832@sit.singaporetech.edu.sg>
2026-01-18 02:12:17 +0800

    Fix: bug fix

--- care-app/next.config.ts
+++ care-app/next.config.ts
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  turbopack: {
+    root: __dirname,
   },
 };
 
